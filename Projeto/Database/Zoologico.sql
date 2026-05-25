1c32b6e8f2e13dc166b842e81b10cc0cea88f7a2
Giovanna Cabral <giovannacabralrmesquita@gmail.com>
2026-05-25 19:40:22 -0300

    Update Zoologico.sql
    acrescentei a tabela consulta veterinario

--- Projeto/Database/Zoologico.sql
+++ Projeto/Database/Zoologico.sql
@@ -4,19 +4,19 @@ use zoologico;
 
 -- Tabel Animal 
 create table Animal (
-	idAnimal int primary key auto_increment,
+	id_Animal int primary key auto_increment,
     nome varchar(45) not null,
     sexo varchar(10) not null, 
     idade int,
     especie varchar(45), 
     peso double (10, 2), 
     id_habitat int, 
-    foreign key (id_habitat) references Habitat(idHabitat)
+    foreign key (id_habitat) references Habitat(id_Habitat)
 );
 
 -- Tabela Habitat
 create table Habitat (
-	idHabitat int primary key auto_increment, 
+	id_Habitat int primary key auto_increment, 
     nome varchar(45) not null,
     clima varchar(45), 
     lotação int, 
@@ -25,18 +25,18 @@ create table Habitat (
 
 -- Tabela Ingresso
 create table Ingresso ( 
-	idIngresso int primary key auto_increment,
+	id_Ingresso int primary key auto_increment,
     data date,
     tipo varchar(45), 
     quantidade int, 
     preco double (10, 2),
     id_visitante int, 
-    foreign key (id_visitante) references Visitante(idVisitante)
+    foreign key (id_visitante) references Visitante(id_Visitante)
 );
 
 -- Tabela Visitante 
 create table Visitante ( 
-	idVisitante int primary key auto_increment,
+	id_Visitante int primary key auto_increment,
     nome varchar(45) not null, 
     idade int not null, 
     numeroVisitante int
@@ -44,15 +44,24 @@ create table Visitante (
 
 -- Tabela Funcionário
 create table Funcionario (
-	idFuncionario int primary key auto_increment,
+	id_Funcionario int primary key auto_increment,
     nome varchar(45) not null, 
     funcao varchar(100) not null, 
     salario double (10, 2)
+);
+
+-- Tabela Consulta Veterinária
+create table ConsultaVeterinaria (
+    id_ConsultaVeterinaria int primary key auto_increment,
+    data date,
+    estado_de_saude varchar(45),
+    id_animal int,
+    id_funcionario int,
+	FOREIGN KEY (id_animal) REFERENCES Animal(id_Animal),
+    FOREIGN KEY (id_funcionario) REFERENCES Funcionario(id_Funcionario)
 );
 
 -- Role para funcionários do Zoológico 
 create role Privilegios;
 grant all on * to Privilegios;
 create user 'DiretorGeral'@'%' identified by 'senha';
-
--- oi gnt 
